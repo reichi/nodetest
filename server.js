f2b4f02b677bc4d8050ac5179ff66046e91f5e6a
reichi <reichi@reichi.com>
2015-03-28 15:27:33 +0100

    environment variablen

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var http = require('http');
-
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Hello Http');
 });
-server.listen(8080);
+server.listen(server_port);
